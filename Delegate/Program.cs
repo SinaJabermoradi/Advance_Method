05d4010bfefbbdff28cf69d05793eca61973874c
SinaJabermoradi <sinajabermoradi313@gmail.com>
2022-08-17 20:30:26 +0430

    Add project files.

---
+++ Delegate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Delegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
