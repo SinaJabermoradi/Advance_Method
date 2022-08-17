2073efd503a1b0a430b1224e8a25c09f6dbf9917
SinaJabermoradi <sinajabermoradi313@gmail.com>
2022-08-17 20:36:57 +0430

    Add

---
+++ Delegate/Test_Delegate.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace Delegate
+{
+    //دلیگیت ، متغییر یا اشاره گری برای نگاه داری (( ساختار یا سیکنیچر )) یا (( آدرس یک متد )) است
+
+    public delegate System.Numerics.BigInteger MathOperation(BigInteger numberOne, BigInteger numberTwo); 
+}
